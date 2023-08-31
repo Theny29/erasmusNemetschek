7f6aed82c620e284ec420d201cd5723dd5335d8f
Daniele <daniele.perottoni@gmail.com>
2023-08-31 14:59:04 +0300

    implement decode

--- App.java
+++ App.java
@@ -2,17 +2,22 @@
 public class App {
     public static void main(String[] args) throws Exception {
 
-        String input = "AAAAAAAAA";
-        String output = "";
+        String inputEncode = "AAAAAVVSSA";
+        String inputDecode = "5A2V2S1A";
 
+        System.out.println(encode(inputEncode));
+        System.out.println(decode(inputDecode));
+    }
+
+    public static String encode(String input) {
         char currLetter = input.charAt(0);
         int count = 0;
+        String output = "";
 
-        for(int i = 0; i < input.length(); i++) {
-            if(currLetter == input.charAt(i)) {
+        for (int i = 0; i < input.length(); i++) {
+            if (currLetter == input.charAt(i)) {
                 count++;
-            }
-            else {
+            } else {
                 output = output + count + currLetter;
                 count = 1;
                 currLetter = input.charAt(i);
@@ -21,6 +26,19 @@ public class App {
 
         output = output + count + currLetter;
 
-        System.out.println(output);
+        return output;
+    }
+
+    public static String decode(String input) {
+        String output = "";
+
+        for (int i = 0; i < input.length(); i += 2) {
+            int num = Integer.parseInt(input.charAt(i)+"");
+            for (int n = 0; n < num; n++) {
+                output += input.charAt(i+1)+"";
+            }
+        }
+
+        return output;
     }
 }
